1b8b6d2afed7fd559059ebaf14d0a7a72bf91f03
premsharma296 <itzpremsharma01@gmail.com>
2026-06-03 15:46:44 +0530

    feat: add transparent navigation header

--- apps/web/components/layout/Navbar.tsx
+++ apps/web/components/layout/Navbar.tsx
@@ -12,10 +12,10 @@ export default function Navbar() {
       left-0
       right-0
       z-50
-      backdrop-blur-xl
-      bg-black/20
-      border-b
-      border-white/10
+      
+      
+      
+      
       "
     >
       <div
